2c2d5493bc1abacb0c04b4483c1758a8d2441f87
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-06-07 08:43:46 +0300

    Basic methods,part1

--- seleniumcourse/src/main/java/FirstTest.java
+++ seleniumcourse/src/main/java/FirstTest.java
@@ -1,21 +1,42 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class FirstTest {
   @Test
-  public void testSearchGoogle (){
+  public void testSearchGoogle() {
 
-   // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-
+    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
     WebDriver driver = new ChromeDriver();
-
     driver.get("http://google.com");
-    WebElement searchBox = driver.findElement(By.name("q"));
+    driver.manage().window().maximize();
+
+    Set<String> oldWindow = driver.getWindowHandles();
+    System.out.println("OldWindow->"+oldWindow);
+
+    ((JavascriptExecutor)driver).executeScript("window.open()");
+    ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+
+    driver.switchTo().window(tabs.get(1));
+    driver.get("http://curs.md");
+
+    Set<String> newWindow = driver.getWindowHandles();
+    System.out.println("NewWindow->"+ newWindow);
+
+    String window = driver.getWindowHandle();
+    System.out.println("Window->"+ window);
+
+    /*WebElement searchBox = driver.findElement(By.name("q"));
     searchBox.sendKeys("table tennis Ma Long");
-    searchBox.submit();
+    searchBox.submit();*/
+
     driver.quit();
 
   }
