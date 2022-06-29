f4e0ee7459b670ab6d62e225e49c4fa523129a96
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-06-29 12:23:31 +0300

    Create new Project

---
+++ ProjectsSelenium/src/main/java/common/Config.java
@@ -0,0 +1,14 @@
+package common;
+
+public class Config {
+  /*
+  * Specify the browser and platform for test
+  * CHROME_WINDOWS
+  * MOZILL_WINDOWS
+  * */
+  public static final String BROWSER_AND_PLATFORM="CHROME_WINDOWS";
+
+  public static final boolean CLEAR_COOKIES = true;
+  public static final boolean HOLD_BROWSER_OPEN = true;
+
+}
---
+++ ProjectsSelenium/src/main/java/constants/Constant.java
@@ -0,0 +1,14 @@
+package constants;
+
+import java.time.Duration;
+
+public class Constant {
+  public static class TimeoutVariabiles{
+    public static final int IMPLICIT_WAIT = 5;
+   // public static final Duration EXPLICIT_WAIT = Duration.ofSeconds(10);
+  }
+
+  public static class Urls{
+    public static final String MOBILE_PAYMENT = "https://epayments.orange.md/?l=2";
+  }
+}
